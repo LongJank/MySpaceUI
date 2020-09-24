32fcac5e9d4715b62cf2c9c92ec2c88d6374d477
LongYsuai <shuailong.yan@videt.cn>
2020-09-24 18:11:01 +0800

    修改登录界面

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="ysl-home">
     <div class="long-home">
-      <div>登录</div>
+      <div class = "login-title">登录</div>
       <div class="login">
-        <el-input placeholder="请输入用户名" v-model="username" clearable>
+        <el-input placeholder="请输入用户名" v-model="username" clearable style="width:80%">
           <i slot="prefix" class="el-input__icon el-icon-user"></i>
         </el-input>
-        <el-input placeholder="请输入密码" v-model="password" show-password>
+        <el-input placeholder="请输入密码" v-model="password" show-password style="width:80%;margin-top:20px">
           <i slot="prefix" class="el-input__icon el-icon-lock"></i>
           <span class="show-pwd" @click="showPwd">
             <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
           </span>
         </el-input>
-        <el-button @click="goHome" type="primary" style="width:100%;margin-bottom:30px;">登录</el-button>
+        <el-button @click="goHome" type="primary" style="width:80%;margin-bottom:30px;margin-top:20px">登录</el-button>
       </div>
     </div>
   </div>
@@ -39,15 +39,18 @@ export default {
   height: 100%;
   text-align: center;
   font-size: 30px;
-  overflow-y:scroll;
+  overflow-y: scroll;
 }
 .long-home {
-  border-radius: 1%;
+  background: #ffffff;
+  box-shadow: 0px 2px 16px 0px rgba(0, 86, 144, 0.12);
+  border-radius: 4px;
   text-align: center;
   width: 20%;
   height: 50%;
   margin: 0 auto;
   margin-top: 200px;
+  padding-top: 3%;
 }
 .login {
   margin-top: 30%;
