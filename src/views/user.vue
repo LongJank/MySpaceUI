8574e0e5712020d400c8b93f06e41540b92409f6
wangshanshan <wangshanshan@jhpy.com>
2021-01-21 15:58:17 +0800

    用户页样式调整

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <el-table :data="tableData" border style="width: 100%">
+    <div class="yslg-contentMain">
+        <el-table :data="tableData">
             <el-table-column prop="id" label="用户ID">
             </el-table-column>
             <el-table-column prop="userNo" label="用户编号">
@@ -13,26 +13,26 @@
             </el-table-column>
             <el-table-column prop="createTime" label="创建时间">
             </el-table-column>
-            <el-table-column label="操作">
+            <el-table-column label="操作" width="180">
                 <template slot-scope="scope">
-                    <el-button size="mini" @click="updateUser = true,updateUserMethon(scope.row)">编辑
+                    <el-button size="mini" @click="updateUserMethon(scope.row)">编辑
                     </el-button>
                     <el-button size="mini" type="primary" @click="lookPassword(scope.row)">查看密码</el-button>
                 </template>
             </el-table-column>
         </el-table>
         <el-dialog title="编辑用户" :visible.sync="updateUser">
-            <el-form :model="form">
-                <el-form-item label="用户编号" :label-width="formLabelWidth">
+            <el-form :model="form" label-width="100px" class="yslg-form">
+                <el-form-item label="用户编号">
                     <el-input v-model="form.userNo" autocomplete="off" :disabled="true"></el-input>
                 </el-form-item>
-                <el-form-item label="用户名" :label-width="formLabelWidth">
+                <el-form-item label="用户名">
                     <el-input v-model="form.userName" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="邮箱" :label-width="formLabelWidth">
+                <el-form-item label="邮箱">
                     <el-input v-model="form.email" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="状态" :label-width="formLabelWidth">
+                <el-form-item label="状态">
                     <el-select v-model="form.status" placeholder="请选择用户状态">
                         <el-option label="启用" value="open"></el-option>
                         <el-option label="停用" value="close"></el-option>
@@ -41,7 +41,7 @@
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="updateUser = false">取 消</el-button>
-                <el-button type="primary" @click="updateUser = false,onSubmitUpdate()">确 定</el-button>
+                <el-button type="primary" @click="onSubmitUpdate()">确 定</el-button>
             </div>
         </el-dialog>
     </div>
@@ -63,7 +63,6 @@
                     email: '',
                     status: ''
                 },
-                formLabelWidth: '120px'
             }
         },
         mounted() {
@@ -71,40 +70,39 @@
         },
         methods: {
             getTableDate() {
-                let url = api.API + "/user/getUserList"
+                let url = api.API + "/user/getUserList";
                 axios.get(url).then((reponse) => {
-                    this.tableData = reponse.data.result
+                    this.tableData = reponse.data.result;
                 })
             },
             lookPassword(row) {
                 console.log(row);
             },
             onSubmitUpdate() {
-                let url = api.API + "/user/updateUser"
-                axios.post(url, {
+                let url = api.API + "/user/updateUser";
+                let params =  {
                     userId: this.form.userId,
                     userName: this.form.userName,
                     email: this.form.email,
                     status: this.form.status == "open" ? 1 : 2
-                }).then((response) => {
+                }
+                axios.post(url, params).then((response) => {
                     if (response.data.errorCode == 200) {
-                        this.$message.success('修改成功')
-                        this.getTableDate()
+                        this.$message.success('修改成功');
+                        this.updateUser = false;
+                        this.getTableDate();
                     } else {
-                        this.$message.error(response.data.message)
+                        this.$message.error(response.data.message);
                     }
                 })
             },
             updateUserMethon(row) {
-                this.form.userId = row.id
-                this.form.userNo = row.userNo
-                this.form.userName = row.userName
-                this.form.email = row.email
-                if (row.status == "启用") {
-                    this.form.status = "open"
-                } else {
-                    this.form.status = "close"
-                }
+                this.updateUser = true;
+                this.form.userId = row.id;
+                this.form.userNo = row.userNo;
+                this.form.userName = row.userName;
+                this.form.email = row.email;
+                this.form.status = row.status == "启用" ? 'open': 'close';
             }
         }
     }
