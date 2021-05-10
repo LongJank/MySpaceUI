d4c502e497cb7b31b42148c9d0950100028b0175
yanshuailong <yanshuailong@sh-tramy.com>
2021-05-10 15:36:43 +0800

    修改

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="content">
       <div class="markdown-body">
-        <Docu />
+        <Docu/>
       </div>
     </div>
   </div>
@@ -13,9 +13,26 @@ import Docu from './docu/README.md'
 import 'highlight.js/styles/github.css'
 // 其他元素使用 github 的样式
 import 'github-markdown-css'
+
 export default {
   components: {
     Docu
   }
 }
 </script>
+
+<style>
+.container {
+  height: 100%;
+  background: #f1f1f1;
+}
+
+.content {
+  min-width: 800px;
+  max-width: 1200px;
+  margin: 0 auto;
+  box-sizing: border-box;
+  padding: 10px 50px;
+  background: #fff;
+}
+</style>
