17924605b0a1adfd703646947b35b4507778b230
yanshuailong <yanshuailong@sh-tramy.com>
2021-05-14 18:29:56 +0800

    修改package

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -1,38 +1,7 @@
 <template>
   <div class="container">
     <div class="content">
-      <div class="markdown-body">
-        <Docu/>
-      </div>
+      111
     </div>
   </div>
 </template>
-
-<script>
-import Docu from './docu/README.md'
-import 'highlight.js/styles/github.css'
-// 其他元素使用 github 的样式
-import 'github-markdown-css'
-
-export default {
-  components: {
-    Docu
-  }
-}
-</script>
-
-<style>
-.container {
-  height: 100%;
-  background: #f1f1f1;
-}
-
-.content {
-  min-width: 800px;
-  max-width: 1200px;
-  margin: 0 auto;
-  box-sizing: border-box;
-  padding: 10px 50px;
-  background: #fff;
-}
-</style>
